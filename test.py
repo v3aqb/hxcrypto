d5ce0f244b33997f7905e70c8680e1ad333fc733
v3aqb <v3aqb@null.com>
2019-11-15 21:10:37 +0800

    update test

--- test.py
+++ test.py
@@ -1,8 +1,28 @@
 
 import os
 import time
-from hxcrypto import encrypt, Encryptor, is_aead
-from hxcrypto.encrypt import AEncryptorAEAD, METHOD_SUPPORTED
+import traceback
+
+from hxcrypto import encrypt, Encryptor, is_aead, method_supported, AEncryptor
+
+
+def test_one(method):
+    data = os.urandom(10240)
+    if is_aead(method):
+        cipher = AEncryptor(b'123456', method, b"ctx")
+        cipher1 = AEncryptor(b'123456', method, b"ctx")
+    else:
+        cipher = Encryptor(b'123456', method)
+        cipher1 = Encryptor(b'123456', method)
+    ct1 = cipher.encrypt(data)
+    cipher1.decrypt(ct1)
+    time_log = time.time()
+    for _ in range(1024):
+        ct1 = cipher.encrypt(data)
+        ct2 = cipher.encrypt(data)
+        cipher1.decrypt(ct1)
+        cipher1.decrypt(ct2)
+    return time.time() - time_log
 
 
 def test():
@@ -18,46 +38,48 @@ def test():
     encrypt.IV_CHECKER = DummyIVChecker(1, 1)
 
     print('encrypt and decrypt 20MB data.')
-    data = os.urandom(10240)
-    lst = sorted(METHOD_SUPPORTED.keys())
+
+    lst = sorted(method_supported.keys())
+    lst = sorted(lst, key=lambda method: is_aead(method))
+    size = max([len(x) for x in lst])
+    format_ = '%-{:d}s %.4fs'.format(size)
+
     for method in lst:
-        if is_aead(method):
-            continue
         try:
-            cipher = Encryptor(b'123456', method)
-            time_log = time.time()
-            for _ in range(1024):
-                ct1 = cipher.encrypt(data)
-                ct2 = cipher.encrypt(data)
-                cipher.decrypt(ct1)
-                cipher.decrypt(ct2)
-            print('%s %ss' % (method, time.time() - time_log))
+            time_used = test_one(method)
+            print(format_ % (method, time_used))
         except Exception as e:
-            print(repr(e))
+            print(traceback.format_exc())
 
-    print('test AE GCM')
-    ae1 = AEncryptorAEAD(b'123456', 'aes-128-gcm', b'ctx')
-    ae2 = AEncryptorAEAD(b'123456', 'aes-128-gcm', b'ctx')
-    ct1 = ae1.encrypt(b'abcde')
-    ct2 = ae1.encrypt(b'fg')
-    print(ae2.decrypt(ct1))
-    print(ae2.decrypt(ct2))
+
+def test_for_result():
+    # disable ivchecker
+    class DummyIVChecker(object):
+        '''DummyIVChecker'''
+        def __init__(self, size, timeout):
+            pass
+
+        def check(self, key, iv):
+            pass
+
+    encrypt.IV_CHECKER = DummyIVChecker(1, 1)
+
+    result = []
+    result.append('encrypt and decrypt 20MB data.')
+
+    lst = sorted(method_supported.keys())
+    lst = sorted(lst, key=lambda method: is_aead(method))
+    size = max([len(x) for x in lst])
+    format_ = '%-{:d}s %.4fs'.format(size)
 
     for method in lst:
-        if is_aead(method):
-            try:
-                cipher = AEncryptorAEAD(b'123456', method, b'ctx')
-                cipher1 = AEncryptorAEAD(b'123456', method, b'ctx')
-                time_log = time.time()
-                for _ in range(1024):
-                    ct1 = cipher.encrypt(data)
-                    ct2 = cipher.encrypt(data)
-                    cipher1.decrypt(ct1)
-                    cipher1.decrypt(ct2)
-                print('%s %ss' % (method, time.time() - time_log))
-            except Exception as e:
-                print(repr(e))
+        try:
+            time_used = test_one(method)
+            result.append(format_ % (method, time_used))
+        except Exception as e:
+            result.append(format_ % method)
+    return '\n'.join(result)
 
 
 if __name__ == '__main__':
-    test()
+    print(test_for_result())
