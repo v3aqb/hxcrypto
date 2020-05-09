2f26cdb310d60375d66814362f4c8e20cc481104
v3aqb <v3aqb@null.com>
2020-05-09 13:46:56 +0800

    ...

--- _generate.py
+++ _generate.py
@@ -5,5 +5,5 @@ for f in glob.glob('./hxcrypto/*.ui'):
     fname = f.replace('\\', '/').split('/')[-1].split('.')[0]
     os.system('pyuic5 %s -o ./hxcrypto/ui_%s.py' % (f, fname))
 
-os.system('pylupdate5 ./hxcrypto/ui_crypto.py -ts -noobsolete ./hxcrypto/translate/zh_CN.ts')
-os.system('lrelease ./hxcrypto/translate/zh_CN.ts')
+# os.system('pylupdate5 ./hxcrypto/ui_crypto.py -ts -noobsolete ./hxcrypto/translate/zh_CN.ts')
+# os.system('lrelease ./hxcrypto/translate/zh_CN.ts')
