3bb45dbc8424d3ac56f00da14f483c978a98f49d
v3aqb <v3aqb@null.com>
2023-04-29 04:52:39 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -14,12 +14,6 @@ setup(
     install_requires=["cryptography >= 3.1", "dmfrbloom", "mmh3"],
     classifiers=[
         'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Topic :: Internet :: Proxy Servers',
+        'Programming Language :: Python :: 3',
     ],
 )
